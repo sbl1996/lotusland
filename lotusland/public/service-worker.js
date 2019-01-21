7111c0f50a70adabda5109a2fcbaf206a19131ff
HrvvI <sbl1996@gmail.com>
2019-01-21 12:21:07 +0800

    fix api proxy error

--- lotusland/public/service-worker.js
+++ lotusland/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7bbfd21d504ce5fb8b90e2db2c9469c3.js"
+  "/precache-manifest.2f8053bf8ab7813313266d5446fd19fb.js"
 );
 
 workbox.clientsClaim();
